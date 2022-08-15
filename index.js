4544bfc45431e10cce726525dc1a87256f4559fd
cyb3rgh05t <flammang.yves@gmail.com>
2022-08-15 19:33:22 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-const DiscordClient = require('./structures/lib/DiscordClient');
-const client = new DiscordClient();
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection
+} = require("discord.js");
+const {
+  Guilds,
+  GuildMembers,
+  GuildMessages,
+  GuildVoiceStates,
+  MessageContent
+} = GatewayIntentBits;
+const {
+  User,
+  Message,
+  GuildMember,
+  ThreadMember
+} = Partials;
 
-client.start();
+const {
+  loadEvents
+} = require("./Handlers/eventHandler");
+const {
+  loadCommands
+} = require("./Handlers/commandHandler");
+const {
+  loadButtons
+} = require("./Handlers/buttonHandler");
 
-module.exports = client;
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages, GuildVoiceStates, MessageContent],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.config = require("./config.json");
+client.commands = new Collection();
+client.buttons = new Collection();
+
+client.login(client.config.token)
+  .then(() => {
+    console.log(`client logged in as ${client.user.username}`);
+    client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
+  })
+  .catch((err) => console.log(err));
